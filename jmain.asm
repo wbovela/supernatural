947dfa3dee3fdb7925f2496fffeb668ee525608c
Wouter Bovelander <wbovela@gmail.com>
2016-05-15 12:08:09 +0200

    Step 7 jumping and gravity
    adds jumping and gravity and some new graphics as well.

--- jmain.asm
+++ jmain.asm
@@ -2,18 +2,6 @@
 !to "jmain.prg",cbm
 
 ;define constants here
-VIC_SPRITE_X_POS        = $d000
-VIC_SPRITE_Y_POS        = $d001
-VIC_SPRITE_X_EXTEND     = $d010
-VIC_SPRITE_ENABLE       = $d015
-VIC_CONTROL             = $d016
-VIC_MEMORY_CONTROL      = $d018
-
-VIC_BACKGROUND_COLOR    = $d021
-VIC_CHARSET_MULTICOLOR_1= $d022
-VIC_CHARSET_MULTICOLOR_2= $d023
-
-CIA_PRA                 = $dd00
 
 ;placeholder for various temp parameters
 PARAM1                  = $03
@@ -27,6 +15,24 @@ ZEROPAGE_POINTER_1      = $17
 ZEROPAGE_POINTER_2      = $19
 ZEROPAGE_POINTER_3      = $21
 ZEROPAGE_POINTER_4      = $23
+
+VIC_SPRITE_X_POS        = $d000
+VIC_SPRITE_Y_POS        = $d001
+VIC_SPRITE_X_EXTEND     = $d010
+VIC_SPRITE_ENABLE       = $d015
+VIC_CONTROL             = $d016
+VIC_MEMORY_CONTROL      = $d018
+VIC_SPRITE_MULTICOLOR   = $d01c
+VIC_SPRITE_MULTICOLOR_1 = $d025
+VIC_SPRITE_MULTICOLOR_2 = $d026
+VIC_SPRITE_COLOR        = $d027
+
+VIC_BORDER_COLOR        = $d020
+VIC_BACKGROUND_COLOR    = $d021
+VIC_CHARSET_MULTICOLOR_1= $d022
+VIC_CHARSET_MULTICOLOR_2= $d023
+
+CIA_PRA                 = $dd00
 
 ;address of the screen buffer
 SCREEN_CHAR             = $CC00
@@ -48,6 +54,12 @@ SPRITE_PLAYER           = SPRITE_BASE + 0
 ;offset from calculated char pos to true sprite pos
 SPRITE_CENTER_OFFSET_X  = 8
 SPRITE_CENTER_OFFSET_Y  = 11
+
+;entries of jump table
+JUMP_TABLE_SIZE         = 11
+
+;entries of fall table
+FALL_TABLE_SIZE         = 10
 
 
 ;level data constants
@@ -139,11 +151,11 @@ LD_LINE_V               = 2
           sta VIC_SPRITE_X_EXTEND
           
           ;init sprite 1 pos
-          lda #1
-          sta PARAM1						; char x
-          lda #2
-          sta PARAM2						; char y
-          ldx #0							; x = sprite index
+          lda #5
+          sta PARAM1
+          lda #4
+          sta PARAM2
+          ldx #0
           
           jsr CalcSpritePosFromCharPos
           
@@ -179,18 +191,57 @@ GameLoop
 ;------------------------------------------------------------
 !zone PlayerControl
 PlayerControl
-          lda #$02
-          bit $dc00
-          bne .NotDownPressed
-          jsr PlayerMoveDown
+
+          lda PLAYER_JUMP_POS				; check our index into jump table
+          bne .PlayerIsJumping				; to see if we're in a jump
+											; can take several game loops
+          jsr PlayerMoveDown				; player always moves down regardless
+          beq .NotFalling					; of joystick input (gravity)
           
-.NotDownPressed          
+          ;player fell one pixel
+          jmp .PlayerFell					; move down checked that we fell
+          
+          ;lda #$02
+          ;bit $dc00
+          ;bne .NotDownPressed
+          ;jsr PlayerMoveDown
+          
+.NotFalling          
+          lda #0							; not falling? then reset fall pos
+          sta PLAYER_FALL_POS
+          
+.NotDownPressed          					; and continue normally
           lda #$01
           bit $dc00
           bne .NotUpPressed
-          jsr PlayerMoveUp
           
+          jmp .PlayerIsJumping				; up pressed means jumping!
+          
+.PlayerFell									; we fell! load our fall index
+          ldx PLAYER_FALL_POS
+          lda FALL_SPEED_TABLE,x			; what's our fall speed?
+          beq .FallComplete				    ; never happens. FALL_SPEED_TABLE no 0
+          sta PARAM5						; store the fall speed
+
+.FallLoop          							; we fall several pixels in 1 loop
+          dec PARAM5						; 
+          beq .FallComplete
+          
+          jsr PlayerMoveDown				; not done falling? move down
+          jmp .FallLoop						; and again
+          
+.FallComplete								; fall complete in this loop?
+          lda PLAYER_FALL_POS				; what's our fall index?
+          cmp #( FALL_TABLE_SIZE - 1 )		; at table's end?
+          beq .FallSpeedAtMax				; yes? then continue as normal
+          
+          inc PLAYER_FALL_POS				; no? increase index for next loop
+		  									; means we can move left during fall
+
+.FallSpeedAtMax
 .NotUpPressed          
+.JumpStopped
+.JumpComplete
           lda #$04
           bit $dc00
           bne .NotLeftPressed
@@ -205,6 +256,37 @@ PlayerControl
 .NotRightPressed
           rts
 
+.PlayerIsJumping
+          inc PLAYER_JUMP_POS
+          lda PLAYER_JUMP_POS
+          cmp #JUMP_TABLE_SIZE
+          bne .JumpOn
+          
+          lda #0
+          sta PLAYER_JUMP_POS
+          jmp .JumpComplete
+          
+.JumpOn                    
+          ldx PLAYER_JUMP_POS
+          
+          lda PLAYER_JUMP_TABLE,x
+          beq .JumpComplete
+          sta PARAM5
+          
+.JumpContinue          
+          jsr PlayerMoveUp
+          beq .JumpBlocked
+          
+          dec PARAM5
+          bne .JumpContinue
+          jmp .JumpComplete
+          
+          
+.JumpBlocked
+          lda #0
+          sta PLAYER_JUMP_POS
+          jmp .JumpStopped
+
 ;------------------------------------------------------------
 ;PlayerMoveLeft
 ;------------------------------------------------------------
@@ -212,52 +294,53 @@ PlayerControl
 PlayerMoveLeft
           ldx #0
           
-          lda SPRITE_CHAR_POS_X_DELTA				; sprite 0, always player (tricky)
-          beq .CheckCanMoveLeft						; is the sprite at a character edge?
-          											; we only check when we're at an edge
-.CanMoveLeft
-          dec SPRITE_CHAR_POS_X_DELTA				; decrease in-character delta
+          lda SPRITE_CHAR_POS_X_DELTA
+          beq .CheckCanMoveLeft
           
-          jsr MoveSpriteLeft						; and move the sprite left
+.CanMoveLeft
+          dec SPRITE_CHAR_POS_X_DELTA
+          
+          jsr MoveSpriteLeft
+          lda #1
           rts
           
 .CheckCanMoveLeft
-          lda SPRITE_CHAR_POS_Y_DELTA				; get the Y in-char delta
-          beq .NoThirdCharCheckNeeded				; are we at a vertical edge?
-          											; if so, check the bottom left first
-          ldy SPRITE_CHAR_POS_Y						; get the char y pos
-          lda SCREEN_LINE_OFFSET_TABLE_LO,y			; get the address of char 1 of line
+          lda SPRITE_CHAR_POS_Y_DELTA
+          beq .NoThirdCharCheckNeeded
+          
+          ldy SPRITE_CHAR_POS_Y
+          lda SCREEN_LINE_OFFSET_TABLE_LO,y
           sta ZEROPAGE_POINTER_1
           lda SCREEN_LINE_OFFSET_TABLE_HI,y
           sta ZEROPAGE_POINTER_1 + 1
 
-          lda SPRITE_CHAR_POS_X						; get the char x pos
-          clc										; add 39 (down and left)
-          adc #39									; check at our feet
+          lda SPRITE_CHAR_POS_X
+          clc
+          adc #39
           tay
           
-          lda (ZEROPAGE_POINTER_1),y				; get the char there
+          lda (ZEROPAGE_POINTER_1),y
           
-          jsr IsCharBlocking						; is it blocking?
+          jsr IsCharBlocking
           bne .BlockedLeft
           
-.NoThirdCharCheckNeeded          					; either from feet or middle UP
-          ldy SPRITE_CHAR_POS_Y						; get the char y pos
-          dey										; decrease (1 line up)
-          lda SCREEN_LINE_OFFSET_TABLE_LO,y			; get the address of char 1 of line
+.NoThirdCharCheckNeeded          
+          ldy SPRITE_CHAR_POS_Y
+          dey
+          lda SCREEN_LINE_OFFSET_TABLE_LO,y
           sta ZEROPAGE_POINTER_1
           lda SCREEN_LINE_OFFSET_TABLE_HI,y
           sta ZEROPAGE_POINTER_1 + 1
           
-          ldy SPRITE_CHAR_POS_X						; get the char x pos
-          dey										; decrease (1 left)
+          ldy SPRITE_CHAR_POS_X
+          dey
           
-          lda (ZEROPAGE_POINTER_1),y				; get the character there
+          lda (ZEROPAGE_POINTER_1),y
           
-          jsr IsCharBlocking						; is it blocking?
+          jsr IsCharBlocking
           bne .BlockedLeft
           
-          tya										; and then 1 line down
+          tya
           clc
           adc #40
           tay
@@ -266,12 +349,13 @@ PlayerMoveLeft
           bne .BlockedLeft
           
           
-          lda #8									; we were always flush
-          sta SPRITE_CHAR_POS_X_DELTA				; so set delta to 8
-          dec SPRITE_CHAR_POS_X						; decrease x position
-          jmp .CanMoveLeft							; can move left will dec delta to 7
+          lda #8
+          sta SPRITE_CHAR_POS_X_DELTA
+          dec SPRITE_CHAR_POS_X
+          jmp .CanMoveLeft
           
 .BlockedLeft
+          lda #0
           rts
 
           
@@ -280,68 +364,65 @@ PlayerMoveLeft
 ;------------------------------------------------------------
 !zone PlayerMoveRight
 PlayerMoveRight
-          ldx #0								; init x to 0 (sprite#?)
+          ldx #0
           
-          lda SPRITE_CHAR_POS_X_DELTA			; get the player x delta
-          beq .CheckCanMoveRight				; if we're flush then check
+          lda SPRITE_CHAR_POS_X_DELTA
+          beq .CheckCanMoveRight
           
-.CanMoveRight									; not at a char edge
-          inc SPRITE_CHAR_POS_X_DELTA			; inc x delta by 1 pixel
+.CanMoveRight
+          inc SPRITE_CHAR_POS_X_DELTA
           
-          lda SPRITE_CHAR_POS_X_DELTA			; load it
-          cmp #8								; full char now?
-          bne .NoCharStep						; no, then nothing
+          lda SPRITE_CHAR_POS_X_DELTA
+          cmp #8
+          bne .NoCharStep
           
-          lda #0								; yes, set x delta to 0
-          sta SPRITE_CHAR_POS_X_DELTA			; store
-          inc SPRITE_CHAR_POS_X					; inc char x position
+          lda #0
+          sta SPRITE_CHAR_POS_X_DELTA
+          inc SPRITE_CHAR_POS_X
           
 .NoCharStep          
-          jsr MoveSpriteRight					; move the sprite
+          jsr MoveSpriteRight
+          lda #1
           rts
           
 .CheckCanMoveRight
-          lda SPRITE_CHAR_POS_Y_DELTA			; first get the y delta
-          beq .NoThirdCharCheckNeeded			; third char check?
+          lda SPRITE_CHAR_POS_Y_DELTA
+          beq .NoThirdCharCheckNeeded
           
-		  ; the code below appears to check what is blocking our feet/
-		  ; lower part of the hit box. One below and to the right.
-          ldy SPRITE_CHAR_POS_Y					; get char y coordinate
-          iny									; increase it (next line)
-          lda SCREEN_LINE_OFFSET_TABLE_LO,y		; get the address
+          ldy SPRITE_CHAR_POS_Y
+          iny
+          lda SCREEN_LINE_OFFSET_TABLE_LO,y
           sta ZEROPAGE_POINTER_1
           lda SCREEN_LINE_OFFSET_TABLE_HI,y
           sta ZEROPAGE_POINTER_1 + 1
 
-          ldy SPRITE_CHAR_POS_X					; get char x coordinate
-          iny									; increase it (right)
+          ldy SPRITE_CHAR_POS_X
+          iny
           
-          lda (ZEROPAGE_POINTER_1),y			; get the character there
+          lda (ZEROPAGE_POINTER_1),y
           
-          jsr IsCharBlocking					; does it block?
+          jsr IsCharBlocking
           bne .BlockedRight
           
 .NoThirdCharCheckNeeded          
 
-		  ; the code below checks if the char to our right and above
-		  ; blocks or not
-          ldy SPRITE_CHAR_POS_Y					; get our char y coordinate
-          dey									; decrease it (1 line up)
-          lda SCREEN_LINE_OFFSET_TABLE_LO,y		; get the screen address
+          ldy SPRITE_CHAR_POS_Y
+          dey
+          lda SCREEN_LINE_OFFSET_TABLE_LO,y
           sta ZEROPAGE_POINTER_1
           lda SCREEN_LINE_OFFSET_TABLE_HI,y
           sta ZEROPAGE_POINTER_1 + 1
           
-          ldy SPRITE_CHAR_POS_X					; get the char x coordinate
-          iny									; increase it (up & right)
-          lda (ZEROPAGE_POINTER_1),y			; get the character there
+          ldy SPRITE_CHAR_POS_X
+          iny
+          lda (ZEROPAGE_POINTER_1),y
           
-          jsr IsCharBlocking					; does it block?
+          jsr IsCharBlocking
           bne .BlockedRight
           
-          tya									; y was our x position
+          tya
           clc
-          adc #40								; add 40 (right & 1 line down)
+          adc #40
           tay
           lda (ZEROPAGE_POINTER_1),y
           jsr IsCharBlocking
@@ -349,7 +430,8 @@ PlayerMoveRight
           
           jmp .CanMoveRight
           
-.BlockedRight
+.BlockedRight 
+          lda #0
           rts
           
 
@@ -376,6 +458,7 @@ PlayerMoveUp
           
 .NoCharStep          
           jsr MoveSpriteUp
+          lda #1
           rts
           
 .CheckCanMoveUp
@@ -418,6 +501,7 @@ PlayerMoveUp
           jmp .CanMoveUp
           
 .BlockedUp
+          lda #0
           rts
           
           
@@ -444,6 +528,7 @@ PlayerMoveDown
           
 .NoCharStep          
           jsr MoveSpriteDown
+          lda #1
           rts
           
 .CheckCanMoveDown
@@ -461,7 +546,7 @@ PlayerMoveDown
           iny
           lda (ZEROPAGE_POINTER_1),y
           
-          jsr IsCharBlocking
+          jsr IsCharBlockingFall
           bne .BlockedDown
           
 .NoSecondCharCheckNeeded          
@@ -477,12 +562,13 @@ PlayerMoveDown
           
           lda (ZEROPAGE_POINTER_1),y
           
-          jsr IsCharBlocking
+          jsr IsCharBlockingFall
           bne .BlockedDown
           
           jmp .CanMoveDown
           
 .BlockedDown
+          lda #0
           rts
           
 
@@ -588,6 +674,26 @@ IsCharBlocking
 
 
 ;------------------------------------------------------------
+;IsCharBlockingFall
+;checks if a char is blocking a fall (downwards)
+;PARAM1 = char_pos_x
+;PARAM2 = char_pos_y
+;returns 1 for blocking, 0 for not blocking
+;------------------------------------------------------------
+!zone IsCharBlockingFall
+IsCharBlockingFall
+          cmp #96
+          bpl .Blocking
+          
+          lda #0
+          rts
+          
+.Blocking
+          lda #1
+          rts
+
+
+;------------------------------------------------------------
 ;CalcSpritePosFromCharPos
 ;calculates the real sprite coordinates from screen char pos
 ;PARAM1 = char_pos_x
@@ -598,41 +704,39 @@ IsCharBlocking
 CalcSpritePosFromCharPos
 
           ;offset screen to border 24,50
-		  ; the first visible dot is at x=24, y=50
-          lda BIT_TABLE,x						; first clear the extend bit
-          eor #$ff								; for this sprite.
+          lda BIT_TABLE,x
+          eor #$ff
           and SPRITE_POS_X_EXTEND
           sta SPRITE_POS_X_EXTEND
-          sta VIC_SPRITE_X_EXTEND				; extend beyond x=$ff bit per sprite
+          sta VIC_SPRITE_X_EXTEND
           
           ;need extended x bit?
-          lda PARAM1							; take the x pos (char)
-          sta SPRITE_CHAR_POS_X,x				; store it in our table
-          cmp #30								; compare to 30
-          bcc .NoXBit							; smaller? no extend, otherwise yes
+          lda PARAM1
+          sta SPRITE_CHAR_POS_X,x
+          cmp #30
+          bcc .NoXBit
           
-          lda BIT_TABLE,x						; get the correct bit value (again)
-          ora SPRITE_POS_X_EXTEND				; or it with the current value
-          sta SPRITE_POS_X_EXTEND				; and store it in our table
-          sta VIC_SPRITE_X_EXTEND				; tell VIC2 as well
+          lda BIT_TABLE,x
+          ora SPRITE_POS_X_EXTEND
+          sta SPRITE_POS_X_EXTEND
+          sta VIC_SPRITE_X_EXTEND
           
 .NoXBit   
           ;calculate sprite positions (offset from border)
-          txa										; sprite index * 2 
-          asl										; for the correct address to store
-          tay										; sprite coordinate in
-          
-		  											; 
-          lda PARAM1								; x position (character)
-          asl										; times 8 (8 bits per char)
-          asl										; 
+          txa
           asl
-          clc										; add the border offset
-          adc #( 24 - SPRITE_CENTER_OFFSET_X )		; sub the center offset
-          sta SPRITE_POS_X,x						; store x in our table
-          sta VIC_SPRITE_X_POS,y					; tell VIC2 as well
+          tay
           
-          lda PARAM2								; same story for y 
+          lda PARAM1
+          asl
+          asl
+          asl
+          clc
+          adc #( 24 - SPRITE_CENTER_OFFSET_X )
+          sta SPRITE_POS_X,x
+          sta VIC_SPRITE_X_POS,y
+          
+          lda PARAM2
           sta SPRITE_CHAR_POS_Y,x
           asl
           asl
@@ -640,10 +744,10 @@ CalcSpritePosFromCharPos
           clc
           adc #( 50 - SPRITE_CENTER_OFFSET_Y )
           sta SPRITE_POS_Y,x
-          sta VIC_SPRITE_Y_POS, y 					; 53249
+          sta 53249,y
           
-          lda #0									; set the x and y delta
-          sta SPRITE_CHAR_POS_X_DELTA,x				; to zero for this sprite
+          lda #0
+          sta SPRITE_CHAR_POS_X_DELTA,x
           sta SPRITE_CHAR_POS_Y_DELTA,x
           rts
 
@@ -962,9 +1066,10 @@ SCREEN_DATA_TABLE
           
           
 LEVEL_1
-          !byte LD_LINE_H,5,5,10,128,9
-          !byte LD_LINE_H,30,12,9,129,9
-          !byte LD_LINE_H,10,19,20,128,9
+          !byte LD_LINE_H,5,5,10,96,13
+          !byte LD_LINE_H,12,7,8,96,13
+          !byte LD_LINE_H,30,12,9,97,13
+          !byte LD_LINE_H,10,19,20,96,13
           !byte LD_LINE_V,7,6,4,128,9
           !byte LD_END
 
@@ -982,6 +1087,18 @@ LEVEL_BORDER_DATA
 
 LEVEL_NR  
           !byte 0
+          
+PLAYER_JUMP_POS
+          !byte 0
+PLAYER_JUMP_TABLE
+          !byte 8,8,7,5,3,2,1,1,1,0,0
+          
+PLAYER_FALL_POS
+          !byte 0
+FALL_SPEED_TABLE
+          !byte 1,1,2,2,3,3,3,3,3,3
+          
+          
 SPRITE_POS_X
           !byte 0,0,0,0,0,0,0,0
 SPRITE_POS_X_EXTEND
